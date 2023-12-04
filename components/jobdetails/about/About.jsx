5115b4c6f58bb5e3a86431e8a77e29000da8bd12
denizataes <denizataes@hotmail.com>
2023-12-04 21:19:40 +0300

    job detail screen implemented

--- components/jobdetails/about/About.jsx
+++ components/jobdetails/about/About.jsx
@@ -3,10 +3,13 @@ import { View, Text } from 'react-native'
 
 import styles from './about.style'
 
-const About = () => {
+const About = ({info}) => {
   return (
-    <View>
-      <Text>About</Text>
+    <View style={styles.container}>
+      <Text style={styles.headText}>About the job:</Text>
+
+      <View style={styles.contextBox}></View>
+        <Text style={styles.contextText}>{info}</Text>
     </View>
   )
 }
--- components/jobdetails/footer/Footer.jsx
+++ components/jobdetails/footer/Footer.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity, Image, Linking, Touchable} from 'react-native'
 
 import styles from './footer.style'
+import {icons} from '../../../constants'
 
-const Footer = () => {
+const Footer = ({ url }) => {
   return (
-    <View>
-      <Text>Footer</Text>
+    <View style ={styles.container}>
+<TouchableOpacity style={styles.likeBtn}>
+    <Image 
+    source={icons.heartOutline}
+    resizeMode = "contain"
+    style={styles.likeBtnImage}
+    />
+</TouchableOpacity>
+
+<TouchableOpacity style={styles.applyBtn}
+onPress={() => Linking.openURL(url)}>
+  <Text style={styles.applyBtnText}>Apply for job</Text>
+</TouchableOpacity>
     </View>
   )
 }
